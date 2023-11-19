b3394acbf6f5588505489384117d7d5092b9bd0a
munyuawilson <wmunyua4@gmail.com>
2023-11-18 23:59:56 -0500

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 import Nav from "./components/Nav";
 
 //require('./components/HelloReact');
-ReactDOM.render(<Nav />, document.getElementById('nav'));
+ReactDOM.render(<Nav />, document.getElementById("hello-react"));
